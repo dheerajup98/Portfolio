64a61496c20724e710271f42960dcb756400397f
dheerajup98 <dheerajup98@gmail.com>
2025-11-18 19:06:57 +0530

    updated

--- src/components/experience/experienceData.tsx
+++ src/components/experience/experienceData.tsx
@@ -1,7 +1,7 @@
 export const experienceRawData = Object.freeze({
 	data: [
 		{
-			companyName: "JS Infosoft",
+			companyName: "Metageeks Technologies",
 			jobRole: "Software Engineer",
 			description: [
 				"Developed and maintained the CRM dashboard, streamlining lead management, client tracking, and appointment scheduling for telecom operations.",
@@ -10,37 +10,9 @@ export const experienceRawData = Object.freeze({
 				"Collaborated with cross-functional teams following Agile methodology, conducted code reviews, and optimized system performance.",
 				"Tech: Typescript, ReactJS, Nextjs, Nodejs, MongoDB, AWS lambda(Serverless), Docker, Redis, RabbitMQ."
 			],
-			startDate: "January 2025",
+			startDate: "November 2024",
 			endDate: "Present",
 			websiteLink: " ",
 		},
-		{
-			companyName: "Metageeks Technologies",
-			jobRole: "Full Stack Developer",
-			description: [
-				"Spearheaded the development of a CRM dashboard that streamlined client management processes, improving team productivity by 30%.",
-				"Integrated CRM with a blockchain website, implementing Solidity-based smart contracts to ensure secure, transparent, and tamper-proof client transactions.",
-				"Collaborated with cross-functional teams to reduce manual workflows by 40%, enhancing customer engagement and operational efficiency.",
-				"Optimized system performance and implemented scalable features, resulting in a 20% faster response time for end-users.",
-				"Tech: Typescript, ReactJS, Nodejs, MongoDB, AI/ML, Generative AI, Solidity.",
-			],
-			startDate: "Oct 2023",
-			endDate: "Dec 2024",
-			websiteLink: "https://www.metageeks.tech/",
-		},
-		{
-			companyName: "Freelancer",
-			jobRole: "Software Developer",
-			description: [
-				"Developed a full-stack ticket booking platform enabling users to book movie and event tickets with real-time seat availability and secure payments.",
-				"Integrated payment gateway APIs (Razorpay/Stripe) to process transactions seamlessly.",
-				"Optimized database queries with MongoDB/PostgreSQL, reducing booking confirmation latency by 30%.",
-				"Tech: Html, Css, Javascript, Reactjs, Nodejs, Mongodb, Prisma(ORM), AWS.",
-			],
-			startDate: "feb 2023",
-			endDate: "sept 2023",
-			websiteLink:
-				"",
-		}
 	],
 });
